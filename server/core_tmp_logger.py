002eced7bea161beca37b2f395f6c0dc67941d43
Yashodhan Pise <technoy@gmail.com>
2014-12-06 15:24:26 +0530

    DB data logger.

--- server/core_tmp_logger.py
+++ server/core_tmp_logger.py
@@ -1,10 +1,23 @@
+import sqlite3
 import subprocess
 import time
 
 class CoreTempLogger:
     def log_temp(self):
         """Logs core temperature to sqlite DB."""
-        pass
+        core_temp = self.get_core_temp()
+        timestamp = int(time.time())
+
+        conn = sqlite3.connect("weather_data.db")
+        try:
+            cur = conn.cursor()
+            cur.execute("INSERT INTO ltemp VALUES (timestamp, core_temp)")
+            conn.commit()
+        except:
+            conn.rollback()
+            print("Transaction failed.")
+        finally:
+            conn.close()
 
     def get_core_temp(self):
         """Get CPU temperature"""
@@ -16,4 +29,5 @@ class CoreTempLogger:
         return float(core_temp)
 
 if __name__=="__main__":
-    print(CoreTempLogger().get_core_temp())
+    #print(CoreTempLogger().get_core_temp())
+    CoreTempLogger().log_temp()
