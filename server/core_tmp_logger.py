478234fc8dabbb049b58fbc0b0560ed9f45f851f
Yashodhan Pise <technoy@gmail.com>
2014-12-06 15:01:41 +0530

    Core temp logger code for testing.

---
+++ server/core_tmp_logger.py
@@ -0,0 +1,19 @@
+import subprocess
+import time
+
+class CoreTempLogger:
+    def log_temp(self):
+        """Logs core temperature to sqlite DB."""
+        pass
+
+    def get_core_tmp(self):
+        """Get CPU temperature"""
+        core_tmp = subprocess.check_output(["/opt/vc/bin/vcgencmd",
+                                            "measure_temp"]).strip()
+        #core_temp value is in format temp=XX.X'C.
+        #Fixing core_temp to required format.
+        core_temp = core_temp.split("=")[1].split("\'")[0]
+        return float(core_temp)
+
+if __name__=="__main__":
+    print(CoreTempLogger().get_core_tmp())
