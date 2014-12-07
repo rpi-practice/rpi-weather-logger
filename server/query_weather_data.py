a7f6d61a195e7a0c2658ee2a537953cc7088e9f6
Yashodhan Pise <technoy@gmail.com>
2014-12-07 23:54:20 +0530

    Decorator function to extract and return the relevant weather data.

--- server/query_weather_data.py
+++ server/query_weather_data.py
@@ -1,4 +1,5 @@
 import httplib
+import json
 
 class QueryWeatherData:
     def __init__(self):
@@ -20,8 +21,21 @@ class QueryWeatherData:
             conn.close()
         return msg
 
+    def decorated_data(self):
+        data = self.make_request()
+        json_data = json.loads(data)
+        #timestamp, loc_id, temp, humid, pres, wind, cloud
+        return {'timestamp':json_data['dt'],
+                'loc_id':json_data['id'],
+                'temp':json_data['main']['temp'],
+                'humidity':json_data['main']['humidity'],
+                'pres':json_data['main']['pres'],
+                'wind':json_data['main']['wind'],
+                'cloud':json_data['main']['cloud'],
+               }
+
 if __name__ == "__main__":
     q = QueryWeatherData()
-    msg = q.make_request()
-    print(msg)
+    print(q.make_request())
+    #q.decorated_data()
 
