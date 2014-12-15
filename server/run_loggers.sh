b451407b218e57e759d76bab95f24680e2ae2175
Yashodhan <technoy@gmail.com>
2014-12-15 12:47:22 +0530

    Modified shell script that runs the loggers.

--- server/run_loggers.sh
+++ server/run_loggers.sh
@@ -1,3 +1,3 @@
-python ./core_temp_logger.py
-python ./web_data_logger.py
+python /home/pi/Projects/temp_logger/server/core_temp_logger.py
+python /home/pi/Projects/temp_logger/server/web_data_logger.py
 
