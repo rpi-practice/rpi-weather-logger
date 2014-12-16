c71b514bd0dafb11e4e901a71a72778443c418ed
Yashodhan <technoy@gmail.com>
2014-12-16 22:44:59 +0530

    Renamed chart.html to index.html.

--- client/server.py
+++ client/server.py
@@ -7,14 +7,16 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'server'))
 from get_data import GetData
 
 class Server(object):
-    @cherrypy.expose
-    def index(self):
-        return open(os.path.join(os.path.dirname(__file__), 'html', 'chart.html'))
+    #@cherrypy.expose
+    #def index(self):
+        #return open(os.path.join(os.path.dirname(__file__), 'html', 'chart.html'))
 
     @cherrypy.expose
     def temp(self):
         return json.dumps(GetData().temp)
 
+sconf = os.path.join(os.path.dirname(__file__), 'server.conf')
+
 if __name__ == '__main__':
-       cherrypy.quickstart(Server(), '/', './client/server.conf')
+       cherrypy.quickstart(Server(), config=sconf)
 
