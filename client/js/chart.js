d4587f4afcd6ab407e05bb2e8414f72c8a2413ce
Yashodhan <technoy@gmail.com>
2014-12-25 22:15:03 +0530

    Change for local time display.

--- client/js/chart.js
+++ client/js/chart.js
@@ -7,6 +7,13 @@ $.ajax({url: 'temp',
         success: function (result, status, xhr) {
                 data = jQuery.parseJSON(result);
                 // Create the chart
+		Highcharts.setOptions({
+        		global: {
+            			timezoneOffset: new Date().getTimezoneOffset(),
+            			useUTC: false
+        		}
+    		});
+
                 $('#container').highcharts('StockChart', {
                         rangeSelector : {
                                 selected : 1
