d55ea2b91388fa2441228b7e7e7228f45aefb9e2
Yashodhan <technoy@gmail.com>
2014-12-25 16:07:54 +0530

    HTML file refactoring.

--- client/js/chart.js
+++ client/js/chart.js
@@ -1,21 +1,30 @@
 function get_data(param) {
-    $.get(param, function (response) {
-        data = jQuery.parseJSON(response);
-  // Create the chart
-        $('#container').highcharts('StockChart', {
-            rangeSelector : {
-                selected : 1
-            },
-            title : {
-                text : 'Weather Data'
-            },
-            series : [{
-                name : 'Temperature',
-                data : data, 
-                tooltip: {
-                valueDecimals: 2
-                }
-            }]
-        });
-    });
+$.ajax({url: 'temp',
+        async: false,
+        contentType: false,
+        processData: false,
+        type: 'GET',
+        success: function (result, status, xhr) {
+                data = jQuery.parseJSON(result);
+                // Create the chart
+                $('#container').highcharts('StockChart', {
+                        rangeSelector : {
+                                selected : 1
+                        },
+                        title : {
+                                text : 'Weather Data'
+                        },
+                        series : [{
+                                name : 'Temperature',
+                                data : data,
+                                tooltip: {
+                                        valueDecimals: 2
+                                }
+                        }]
+                });
+        },
+        error: function(xhr, status, error){
+                alert(status + "::" + error);
+        }});
 }
+
